e0d88ace12528c05aa3685eb5a768a0785db5d0e
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2025-07-21 01:05:01 +0800

    update skin
    (cherry picked from commit e2d73dda40cfe9b51756a07e065b262dd58567e0)

--- resources/views/frontend/head.blade.php
+++ resources/views/frontend/head.blade.php
@@ -10,7 +10,7 @@
     <title>{{setting("site_name")}}</title>
 
     <!-- favicon -->
-    <link rel="icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+    <link rel="icon" type="image/x-icon" href="{{Storage::url(setting('site_logo', 'default value'))}}">
 
     <!-- css -->
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
@@ -21,16 +21,20 @@
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
+    @php
+    $mapbg = asset('assets/img/slider/map-bg.png');
+    @endphp
     <style>
-.searh-wrapper {
-    padding: 10px 41px 29px;
-    background-color: #1a1e37;
-    /* margin: 20px; */
-    background-size: contain;
-    background-image: url(http://filament.test/filament-cms/public/assets/img/slider/map-bg.png);
-}
-        .feature-item{
+        .searh-wrapper {
+            padding: 10px 41px 29px;
+            background-color: #116e63;
+            /* margin: 20px; */
+            background-size: contain;
+            background-image: url(<?php echo $mapbg ?>);
+
+        }
+
+        .feature-item {
             display: flex;
             flex-direction: column;
             text-align: center;
@@ -38,6 +42,7 @@
             border-radius: 20px;
             padding: 20px 19px;
         }
+
         .counter-box .icon {
             position: relative;
             text-align: center;
@@ -49,6 +54,7 @@
             background: var(--theme-color2);
             border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
         }
+
         .counter-box .title {
             color: var(--color-white);
             margin-top: 20px;
@@ -56,154 +62,194 @@
             font-weight: 600;
             text-transform: capitalize;
         }
+
         .about-img .img-1 {
             border-radius: 41px;
         }
+
         .event-item {
-    position: relative;
-    background: var(--color-white);
-    border-radius: 20px;
-    padding: 20px 20px 20px 20px;
-    margin-bottom: 25px;
-    box-shadow: var(--box-shadow);
-}
-.event-item .event-location .course-level {
-    font-size: 13px;
-    font-weight: 500;
-    color: var(--color-white);
-    font-family: var(--font-secondary);
-    background-color: #cd201f;
-    border-radius: 3px;
-    padding: 1px 10px;
-    margin-bottom: 15px;
-    display: inline-block;
-}
+            position: relative;
+            background: var(--color-white);
+            border-radius: 20px;
+            padding: 20px 20px 20px 20px;
+            margin-bottom: 25px;
+            box-shadow: var(--box-shadow);
+        }
 
-.event-item .event-location .course-level2 {
-    font-size: 13px;
-    font-weight: 500;
-    color: var(--color-white);
-    font-family: var(--font-secondary);
-    background-color: var(--theme-color);
-    border-radius: 3px;
-    padding: 1px 10px;
-    margin-bottom: 15px;
-    display: inline-block;
-}
+        .event-item .event-location .course-level {
+            font-size: 13px;
+            font-weight: 500;
+            color: var(--color-white);
+            font-family: var(--font-secondary);
+            background-color: #cd201f;
+            border-radius: 3px;
+            padding: 1px 10px;
+            margin-bottom: 15px;
+            display: inline-block;
+        }
 
-.theme-btn-read-download {
-    font-size: 14px;
-    color: var(--color-white);
-    padding: 14px 20px;
-    transition: var(--transition);
-    text-transform: uppercase;
-    position: relative;
-    border-radius: 5px;
-    font-weight: 600;
-    letter-spacing: 1px;
-    cursor: pointer;
-    text-align: center;
-    overflow: hidden;
-    border: none;
-    background: #c0c0c0;
-    box-shadow: var(--box-shadow);
-    z-index: 1;
-}
+        .event-item .event-location .course-level2 {
+            font-size: 13px;
+            font-weight: 500;
+            color: var(--color-white);
+            font-family: var(--font-secondary);
+            background-color: var(--theme-color);
+            border-radius: 3px;
+            padding: 1px 10px;
+            margin-bottom: 15px;
+            display: inline-block;
+        }
 
-.team-item {
-    position: relative;
-    background: var(--color-white);
-    padding: 15px;
-    margin-bottom: 25px;
-    border-radius: 20px;
-    overflow: hidden;
-    box-shadow: var(--box-shadow);
-    transition: var(--transition);
-}
+        .theme-btn-read-download {
+            font-size: 14px;
+            color: var(--color-white);
+            padding: 14px 20px;
+            transition: var(--transition);
+            text-transform: uppercase;
+            position: relative;
+            border-radius: 5px;
+            font-weight: 600;
+            letter-spacing: 1px;
+            cursor: pointer;
+            text-align: center;
+            overflow: hidden;
+            border: none;
+            background: #c0c0c0;
+            box-shadow: var(--box-shadow);
+            z-index: 1;
+        }
 
-.team-img img {
-    height: 300px;
-    width: 100%;
-    object-fit: cover;
+        .team-item {
+            position: relative;
+            background: var(--color-white);
+            padding: 15px;
+            margin-bottom: 25px;
+            border-radius: 20px;
+            overflow: hidden;
+            box-shadow: var(--box-shadow);
+            transition: var(--transition);
+        }
 
-    border-radius: 10px;
-}
+        .team-img img {
+            height: 300px;
+            width: 100%;
+            object-fit: cover;
 
-.video-content::before {
-    content: "";
-    position: absolute;
-    background: rgba(3, 2, 7, .2);
-    border-radius: 10px;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-}
+            border-radius: 10px;
+        }
 
-.video-content {
-    position: relative;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    border-radius: 10px;
-}
+        .video-content::before {
+            content: "";
+            position: absolute;
+            background: rgba(3, 2, 7, .2);
+            border-radius: 10px;
+            width: 100%;
+            height: 100%;
+            left: 0;
+            top: 0;
+        }
 
-   .banner-text {
-    position: absolute;
-    top: 27%;
-    z-index: 1;
-    width: 100%;
-    text-align: center;
-}
-.banner-form-box {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    width: 80%;
-    background-color: #fff;
-    border-radius: 4px;
-    box-shadow: 0 2px 12px rgba(0, 0, 0, .09);
-    margin: 20px auto 0;
-}
-.banner-form-input {
-    padding: 10px 12px;
-    border-right: 1px solid #e4e4e4;
-    -webkit-box-flex: 1;
-    -ms-flex: 1;
-    flex: 1;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    position: relative;
-}
+        .video-content {
+            position: relative;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            border-radius: 10px;
+        }
 
-.banner-form-input input {
-    padding: 5px 10px;
-    width: 100%;
-    border: medium none;
-}
+        .banner-text {
+            position: absolute;
+            top: 27%;
+            z-index: 1;
+            width: 100%;
+            text-align: center;
+        }
 
-.banner-form-input button {
-    position: absolute;
-    left: 0;
-    top: 0;
-    height: 100%;
-    margin: 0;
-    width: 100%;
-    border: medium none;
-    cursor: pointer;
-    background: var(--theme-color2);
-    color: #fff;
-    border-radius: 0px 4px 4px 0px;
-    -webkit-transition: all 0.4s ease 0s;
-    transition: all 0.4s ease 0s;
-}
+        .banner-form-box {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            width: 80%;
+            background-color: #fff;
+            border-radius: 4px;
+            box-shadow: 0 2px 12px rgba(0, 0, 0, .09);
+            margin: 20px auto 0;
+        }
 
+        .banner-form-input {
+            padding: 10px 12px;
+            border-right: 1px solid #e4e4e4;
+            -webkit-box-flex: 1;
+            -ms-flex: 1;
+            flex: 1;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            align-items: center;
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            position: relative;
+        }
 
+        .banner-form-input input {
+            padding: 5px 10px;
+            width: 100%;
+            border-radius: 7px;
+            border: var(--bs-border-width) solid var(--bs-border-color);
+        }
 
+        .banner-form-input button {
+            position: relative;
+            /* left: 0; */
+            /* top: 0; */
+            height: 37px;
+            margin: 0;
+            width: 100%;
+            border: medium none;
+            cursor: pointer;
+            background: var(--theme-color2);
+            color: #fff;
+            -webkit-transition: all 0.4s ease 0s;
+            transition: all 0.4s ease 0s;
+        }
+
+        @media screen and (min-width:200px) and (max-width:700px) {
+            .banner-form-box {
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: flex;
+                flex-direction: column;
+                width: 100%;
+                background-color: #fff;
+                border-radius: 4px;
+                box-shadow: 0 2px 12px rgba(0, 0, 0, .09);
+                margin: 20px auto 0;
+            }
+
+            .searh-wrapper {
+                padding: 10px;
+                background-color: #116e63;
+                /* margin: 20px; */
+                background-size: contain;
+                height: 39vh;
+            }
+
+            .banner-form-input button {
+                position: absolute;
+                left: 0;
+                top: 0;
+                height: 37px;
+                margin: 0;
+                width: 100%;
+                border: medium none;
+                cursor: pointer;
+                background: var(--theme-color2);
+                color: #fff;
+                border-radius: 0px 4px 4px 0px;
+                -webkit-transition: all 0.4s ease 0s;
+                transition: all 0.4s ease 0s;
+            }
+        }
     </style>
 
 </head>
--- resources/views/frontend/nav/header.blade.php
+++ resources/views/frontend/nav/header.blade.php
@@ -1,108 +1,118 @@
 <header class="header">
 
-        <!-- header top -->
-        <div class="header-top">
-            <div class="container">
-                <div class="header-top-wrap">
-                    <div class="header-top-left">
-                        <p class="header-top-news">Selamat Datang di Website Resmi JDIH Kabupaten Bengkalis</p>
+    <!-- header top -->
+    <div class="header-top">
+        <div class="container">
+            <div class="header-top-wrap">
+                <div class="header-top-left">
+                    <div class="header-top-social">
+                        <span>Ikuti Kami : </span>
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-whatsapp"></i></a>
                     </div>
-                    <!-- <div class="header-top-right">
-                        <div class="header-top-menu">
-                            <a href="#">Login</a>
-                            <a href="#">Safety & Security</a>
-                            <a href="#">Students</a>
-                            <a href="#">Help Desk</a>
-                            <a href="#">Alumni</a>
-                            <a href="#">Faculty</a>
-                            <a href="#">Sitemap</a>
-                        </div>
-                    </div> -->
+                </div>
+                <div class="header-top-right">
+                    <div class="header-top-contact">
+                        <ul>
+                            <!-- <li>
+                                <a href="#"><i class="far fa-location-dot"></i> {{setting('site_address')}}</a>
+                            </li> -->
+                            <li>
+                                <a href="mailto:{{setting('site_email')}}"><i class="far fa-envelopes"></i> {{setting('site_email')}}</a>
+                            </li>
+                            <li>
+                                <a href="tel:{{setting("site_phone")}}"><i class="far fa-phone-volume"></i>{{setting("site_phone")}}</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="main-navigation">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container position-relative">
-                    <a class="navbar-brand" href="{{ route('home') }}">
-                        <img src="{{Storage::url(setting('site_logo', 'default value'))}}">
-                    </a>
-                    <div class="mobile-menu-right">
+    </div>
+    <div class="main-navigation">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container position-relative">
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="{{Storage::url(setting('site_logo', 'default value'))}}">
+                </a>
+                <div class="mobile-menu-right">
+                    <div class="search-btn">
+                        <button type="button" class="nav-right-link search-box-outer"><i
+                                class="far fa-search"></i></button>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-mobile-icon"><i class="far fa-bars"></i></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="main_nav">
+                    <ul class="navbar-nav">
+                        @foreach(Biostate\FilamentMenuBuilder\Models\MenuItem::whereHas('menu', function($query) {
+                        $query->where('name', 'WEBSITE');
+                        })->whereNull('parent_id')->get() as $menuItem)
+                        @if($menuItem->children->isEmpty())
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->url() == $menuItem->link ? 'active' : '' }}"
+                                href="{{ $menuItem->link }}"
+                                target="{{ $menuItem->target }}">
+                                {{ $menuItem->name }}
+                            </a>
+                        </li>
+                        @else
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle {{ request()->url() == $menuItem->link ? 'active' : '' }}"
+                                href="{{ $menuItem->link }}"
+                                data-bs-toggle="dropdown"
+                                target="{{ $menuItem->target }}">
+                                {{ $menuItem->name }}
+                            </a>
+                            <ul class="dropdown-menu fade-down">
+                                @foreach($menuItem->children as $child)
+                                @if($child->children->isEmpty())
+                                <li>
+                                    <a class="dropdown-item {{ request()->url() == $child->link ? 'active' : '' }}"
+                                        href="{{ $child->link }}"
+                                        target="{{ $child->target }}">
+                                        {{ $child->name }}
+                                    </a>
+                                </li>
+                                @else
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle {{ request()->url() == $child->link ? 'active' : '' }}"
+                                        href="{{ $child->link }}"
+                                        target="{{ $child->target }}">
+                                        {{ $child->name }}
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        @foreach($child->children as $grandChild)
+                                        <li>
+                                            <a class="dropdown-item {{ request()->url() == $grandChild->link ? 'active' : '' }}"
+                                                href="{{ $grandChild->link }}"
+                                                target="{{ $grandChild->target }}">
+                                                {{ $grandChild->name }}
+                                            </a>
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </li>
+                                @endif
+                                @endforeach
+                            </ul>
+                        </li>
+                        @endif
+                        @endforeach
+                    </ul>
+                    <div class="nav-right">
                         <div class="search-btn">
                             <button type="button" class="nav-right-link search-box-outer"><i
                                     class="far fa-search"></i></button>
                         </div>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-mobile-icon"><i class="far fa-bars"></i></span>
-                        </button>
-                    </div>
-                    <div class="collapse navbar-collapse" id="main_nav">
-                        <ul class="navbar-nav">
-                            @foreach(Biostate\FilamentMenuBuilder\Models\MenuItem::whereHas('menu', function($query) {
-                                $query->where('name', 'WEBSITE');
-                            })->whereNull('parent_id')->get() as $menuItem)
-                                @if($menuItem->children->isEmpty())
-                                    <li class="nav-item">
-                                        <a class="nav-link {{ request()->url() == $menuItem->link ? 'active' : '' }}"
-                                           href="{{ $menuItem->link }}"
-                                           target="{{ $menuItem->target }}">
-                                            {{ $menuItem->name }}
-                                        </a>
-                                    </li>
-                                @else
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle {{ request()->url() == $menuItem->link ? 'active' : '' }}"
-                                           href="{{ $menuItem->link }}"
-                                           data-bs-toggle="dropdown"
-                                           target="{{ $menuItem->target }}">
-                                            {{ $menuItem->name }}
-                                        </a>
-                                        <ul class="dropdown-menu fade-down">
-                                            @foreach($menuItem->children as $child)
-                                                @if($child->children->isEmpty())
-                                                    <li>
-                                                        <a class="dropdown-item {{ request()->url() == $child->link ? 'active' : '' }}"
-                                                           href="{{ $child->link }}"
-                                                           target="{{ $child->target }}">
-                                                            {{ $child->name }}
-                                                        </a>
-                                                    </li>
-                                                @else
-                                                    <li class="dropdown-submenu">
-                                                        <a class="dropdown-item dropdown-toggle {{ request()->url() == $child->link ? 'active' : '' }}"
-                                                           href="{{ $child->link }}"
-                                                           target="{{ $child->target }}">
-                                                            {{ $child->name }}
-                                                        </a>
-                                                        <ul class="dropdown-menu">
-                                                            @foreach($child->children as $grandChild)
-                                                                <li>
-                                                                    <a class="dropdown-item {{ request()->url() == $grandChild->link ? 'active' : '' }}"
-                                                                       href="{{ $grandChild->link }}"
-                                                                       target="{{ $grandChild->target }}">
-                                                                        {{ $grandChild->name }}
-                                                                    </a>
-                                                                </li>
-                                                            @endforeach
-                                                        </ul>
-                                                    </li>
-                                                @endif
-                                            @endforeach
-                                        </ul>
-                                    </li>
-                                @endif
-                            @endforeach
-                        </ul>
-                        <div class="nav-right">
-                            <div class="search-btn">
-                                <button type="button" class="nav-right-link search-box-outer"><i
-                                        class="far fa-search"></i></button>
-                            </div>
 
-                        </div>
                     </div>
                 </div>
-            </nav>
-        </div>
-    </header>
+            </div>
+        </nav>
+    </div>
+</header>
