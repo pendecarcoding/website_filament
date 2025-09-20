1078ec934fff6e6141b06d949d28933a38fa3572
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2025-09-21 00:37:22 +0800

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -34,16 +34,23 @@ class ApiController extends Controller
 
     public function productHukumJdih(Request $request)
     {
-        if ($request->jenis = 'perbub') {
-            $produkHukum = ProdukHukum::where('category_id', '3')->get();
-        } elseif ($request->jenis == 'perda') {
-            $produkHukum = ProdukHukum::where('category_id', '5')->get();
-        } else {
-            $produkHukum = ProdukHukum::all();
+        switch ($request->get('jenis')) {
+            case 'perbub':
+                $produkHukum = ProdukHukum::where('category_id', 3)->get();
+                break;
+
+            case 'perda':
+                $produkHukum = ProdukHukum::where('category_id', 5)->get();
+                break;
+
+            default:
+                $produkHukum = ProdukHukum::all();
+                break;
         }
 
         return response()->json($produkHukum);
     }
+
 
     public function selayangPandang(Request $r)
     {
